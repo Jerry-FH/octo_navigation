444c24ba73f6087946bc5b8228db9d8ff69ccda8
RRL-ALeRT <alert@fh-aachen.de>
2025-07-13 18:21:14 +0200

    lin_vel parameters, dist tolerance, frequency

--- octo_controller/src/octo_controller.cpp
+++ octo_controller/src/octo_controller.cpp
@@ -93,7 +93,7 @@ uint32_t OctoController::computeVelocityCommands(const geometry_msgs::msg::PoseS
   pursuit_index_ = new_index;
 
   // Use the computed velocities.
-  cmd_vel.twist.linear.x = linear_vel*.5;
+  cmd_vel.twist.linear.x = linear_vel;
   cmd_vel.twist.angular.z = angular_vel;
   cmd_vel.header.stamp = node_->now();
   if (cancel_requested_) {
@@ -188,7 +188,7 @@ bool OctoController::isGoalReached(double dist_tolerance, double angle_tolerance
   double dy = goal_pos_.pose.position.y - current_pose_.pose.position.y;
   double dz = goal_pos_.pose.position.z - current_pose_.pose.position.z;
   double goal_distance = std::sqrt(dx * dx + dy * dy + dz * dz);
-  dist_tolerance = 0.56;
+  dist_tolerance = 0.075;
   RCLCPP_INFO(node_->get_logger(), "Goal distance: %f,  dist_tolerance: %f", goal_distance, dist_tolerance);
   // Calculate the angle difference between the robot's current orientation and the goal orientation
   double angle = std::acos(std::cos(current_pose_.pose.orientation.z - goal_pos_.pose.orientation.z));
